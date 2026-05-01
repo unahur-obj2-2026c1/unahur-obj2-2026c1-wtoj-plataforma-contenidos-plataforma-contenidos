d03f4c68ab76af57d035b243688de2563d08a782
Mauricio Pintos <83178447+mauriciopintos@users.noreply.github.com>
2026-04-30 23:03:44 -0300

    Revert "Solucion refactor"

--- src/main/java/ar/edu/unahur/obj2/w2j/planes/PlanStrategy.java
+++
@@ -1,8 +0,0 @@
-package ar.edu.unahur.obj2.w2j.planes;
-
-import ar.edu.unahur.obj2.w2j.usuarios.Usuario;
-
-public interface PlanStrategy {
-    Double costoPlan(Usuario usuario);
-    void actualizaCostoPlan(Double nuevoValor);
-}
